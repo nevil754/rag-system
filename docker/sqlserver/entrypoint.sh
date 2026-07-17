e9f19e18e1059f0c4ee7f753ba17bce3812f9edc
Nelvison Benedetto <nelvisonb@compet-e.it>
2026-07-17 16:49:28 +0200

    added #!/bin/bash in entrypoint.sh e per sqlserver added -C nell'health check e in entrypoint.sh

--- docker/sqlserver/entrypoint.sh
+++ docker/sqlserver/entrypoint.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 set -e
 
 echo "Attendo SQL Server..."
@@ -5,6 +6,7 @@ until /opt/mssql-tools18/bin/sqlcmd \
     -S localhost \
     -U SA \
     -P "$SA_PASSWORD" \
+    -C \
     -Q "SELECT 1" > /dev/null 2>&1; do
     sleep 2
 done
@@ -14,6 +16,7 @@ echo "SQL Server pronto. Eseguo init.sql..."
     -S localhost \
     -U SA \
     -P "$SA_PASSWORD" \
+    -C \
     -i /docker-entrypoint-initdb.d/init.sql \
     -b
 
